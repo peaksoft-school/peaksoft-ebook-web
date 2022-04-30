e645ae6e5e334aa4f942194ccf65da8d3134b4ee
gulnaz <janbosunova.kk@gmail.com>
2022-04-30 19:11:34 +0300

    fix radioButton component

--- src/components/UI/RadioButton/RadioButton.jsx
+++ src/components/UI/RadioButton/RadioButton.jsx
@@ -2,10 +2,10 @@ import styled from '@emotion/styled'
 import { forwardRef } from 'react'
 
 export const RadioButton = forwardRef((props, ref) => {
-   return <RadioInput {...props} ref={ref} type="radio" name="gender" />
+   return <RadioInput {...props} ref={ref} type="radio" />
 })
 const RadioInput = styled.input`
-   &[name='gender'] {
+   & {
       accent-color: #ff4c00;
       width: 26px;
       height: 26px;
