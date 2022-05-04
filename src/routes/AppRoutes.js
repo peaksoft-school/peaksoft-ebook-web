2a059a556785c3cd6a4d22dcdd598bd288958144
Gulniza <gulnizajanyshova@gmail.com>
2022-05-04 12:35:52 +0300

    fix modal ui -component

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { ErrorNotification } from '../components/UI/Notification/ErrorNotification'
 
 export const AppRoutes = () => {
    return (
       <Routes>
-         <Route path="*" element={<ErrorNotification />} />
+         <Route path="*" element={<div>Hello World</div>} />
       </Routes>
    )
 }
