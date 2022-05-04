a7989ecd727e017abb087811fdf735d134750d84
Gulniza <gulnizajanyshova@gmail.com>
2022-05-04 20:08:46 +0300

    success-error-modal

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { ErrorModal } from '../components/UI/Modals/ErrorModal'
 
 export const AppRoutes = () => {
    return (
       <Routes>
-         <Route path="*" element={<ErrorModal isOpen />} />
+         <Route path="*" element={<div>Hello World</div>} />
       </Routes>
    )
 }
