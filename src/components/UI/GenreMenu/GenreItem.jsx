68f53a5f1529f2eefc89dffce99ef400525876a0
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2022-05-04 11:06:10 +0600

    add genre menu ui component

---
+++ src/components/UI/GenreMenu/GenreItem.jsx
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled/macro'
+import React from 'react'
+
+export const GenreItem = ({ genre, count }) => {
+   const convertTo21Symbols = () => {
+      if (genre.length > 22) {
+         const genreAsArray = genre.split('')
+         const convertedGenre = `${genreAsArray.splice(0, 19).join('')}...`
+         return convertedGenre
+      }
+      return genre
+   }
+   return (
+      <GenreItemContainer>
+         <StyledGenre>{convertTo21Symbols()}</StyledGenre>
+         <StyledCount>{count}</StyledCount>
+      </GenreItemContainer>
+   )
+}
+const StyledCount = styled.span`
+   font-weight: 600;
+   font-size: 14px;
+   line-height: 19px;
+   letter-spacing: 0.1em;
+   color: #a5a5a5;
+`
+const StyledGenre = styled.span`
+   transition: color 0.2s ease;
+   font-size: 16px;
+   line-height: 22px;
+   letter-spacing: 0.1em;
+   color: #222222;
+   font-weight: 400;
+   max-width: 233px;
+   height: 22px;
+   overflow: hidden;
+`
+const GenreItemContainer = styled.p`
+   display: flex;
+   justify-content: space-between;
+   padding-bottom: 3px;
+   &:hover {
+      padding-bottom: 2px;
+      border-bottom: 1px solid #ff4c00;
+   }
+   &:hover ${StyledCount} {
+      color: #ff4c00;
+   }
+   &:hover ${StyledGenre} {
+      color: #ff4c00;
+      font-weight: 600;
+   }
+   cursor: pointer;
+   font-family: 'Open Sans';
+   margin: 7.5px 0;
+   min-width: 328px;
+`
---
+++ src/components/UI/GenreMenu/GenreMenu.jsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import React from 'react'
+import { GenreItem } from './GenreItem'
+
+export const GenreMenu = ({ genres }) => {
+   return (
+      <GenreMenuContainer>
+         {genres.map((genre) => (
+            <GenreItem
+               key={Math.random().toString()}
+               genre={genre.genre}
+               count={genre.count}
+            />
+         ))}
+      </GenreMenuContainer>
+   )
+}
+
+const GenreMenuContainer = styled.div`
+   width: fit-content;
+   background: #f8f8f8;
+   display: grid;
+   grid-template-columns: repeat(3, 1fr);
+   grid-column-gap: 108px;
+   padding: 31px 30px;
+   min-width: 1280px;
+   min-height: 445px;
+`
