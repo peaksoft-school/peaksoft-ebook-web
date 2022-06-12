e47b04358a017ce63bf4049e188dd0fd491d2ba2
Gulniza <gulnizajanyshova@gmail.com>
2022-06-12 14:42:43 +0300

    add-become-vendor

--- src/routes/ClientRoutes.js
+++ src/routes/ClientRoutes.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { DEFAULT_ROUTES } from '../utils/constants/routes'
+import { BECOME_VENDOR_ROUTES, DEFAULT_ROUTES } from '../utils/constants/routes'
 import { UserLayout } from '../layout/UserLayout'
+import { BecomeVendor } from '../components/User/BecomeVendor'
 
 export const ClientRoutes = () => {
    return (
@@ -14,7 +15,10 @@ export const ClientRoutes = () => {
                </div>
             }
          />
-
+         <Route
+            path={BECOME_VENDOR_ROUTES.BECOME_VENDOR.PATH}
+            element={<BecomeVendor />}
+         />
          <Route
             path={DEFAULT_ROUTES.NOT_FOUND.PATH}
             element={<div>Страница не найдена</div>}
